b8f833541139550ab3aa5be66697fee19131a233
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2020-07-10 14:27:49 -0400

    sign up part is done.

--- src/pages/sign-In/sign-In.component.jsx
+++ src/pages/sign-In/sign-In.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./sign-In.style.scss";
 import SignIn from "../../components/signIn/signIn.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 const SignInup = () => {
   return (
-    <div className="sing-in-and-sign-up">
+    <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
