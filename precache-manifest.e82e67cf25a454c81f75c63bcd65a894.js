fc8f104922d34b0a95570acd61b8f38a3ecd7ce0
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2020-07-30 12:18:06 -0400

    Updates

---
+++ precache-manifest.e82e67cf25a454c81f75c63bcd65a894.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bd519f6143726e8a46b1c637d6b206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "890bd037fcb61f8bebf0",
+    "url": "/static/css/main.f8221e13.chunk.css"
+  },
+  {
+    "revision": "de29f09e0d8a63f18475",
+    "url": "/static/js/2.e831cf07.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.e831cf07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890bd037fcb61f8bebf0",
+    "url": "/static/js/main.a0442677.chunk.js"
+  },
+  {
+    "revision": "afcba58eda30d49d2fa7",
+    "url": "/static/js/runtime-main.bbd6dda4.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
